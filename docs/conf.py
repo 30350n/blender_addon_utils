32a86d141a7cc579d531fb5a90929c368a8999a2
Bobbe <maxschlecht@web.de>
2023-02-24 06:02:12 +0100

    Fix docs conf path

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path("../..").resolve()))
+sys.path.insert(0, str((Path(__file__).resolve().parents[2])))
 
 project = "blender_addon_utils"
 author = "30350n"
