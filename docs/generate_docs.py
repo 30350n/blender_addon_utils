cc6887235a008915a69b20fc55b67b7f64c01159
Bobbe <maxschlecht@web.de>
2023-03-31 02:18:45 +0200

    Fix generate_docs cwd

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -1,3 +1,6 @@
 import subprocess
+from pathlib import Path
+
 subprocess.check_call(
-    "python -m sphinx -T -E -b html -d _build/doctrees -D language=en . html", shell=True)
+    "python -m sphinx -T -E -b html -d _build/doctrees -D language=en . html",
+    cwd=Path(__file__).parent)
