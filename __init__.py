d25302a3f004cee67cd70aaebbf7544d72ffa4e0
Bobbe <maxschlecht@web.de>
2024-01-23 23:14:48 +0100

    Add __all__ definition to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from .register import register_modules_factory, add_dependencies
 from .error_helper import ErrorHelper
 
+__all__ = ["register_modules_factory", "add_dependencies", "ErrorHelper"]
+
 __version__ = "0.2"
